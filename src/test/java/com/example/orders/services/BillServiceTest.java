27c6abb6b0c55b1aa5777b1eb749cb55a8a71a9c
mfaherold1998 <mfaherold1998@gmail.com>
2024-10-08 11:24:30 +0200

    Test funziona, listo para merge

--- src/test/java/com/example/orders/services/BillServiceTest.java
+++ src/test/java/com/example/orders/services/BillServiceTest.java
@@ -3,10 +3,13 @@ package com.example.orders.services;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class BillServiceTest {
 
     @Test
     void test_trur() {
+        assertTrue(true);
     }
 }
