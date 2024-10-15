bc579184f07e6bb1841157f6335db89eee692869
mfaherold1998 <mfaherold1998@gmail.com>
2024-10-15 14:29:40 +0200

    BillControllerMockBeanTest funciona

--- src/test/java/com/example/orders/controller/BillControllerMockBeanTest.java
+++ src/test/java/com/example/orders/controller/BillControllerMockBeanTest.java
@@ -1,9 +1,13 @@
 package com.example.orders.controller;
 
 import com.example.orders.dto.BillDto;
+import com.example.orders.exceptions.NotFoundException;
 import com.example.orders.repository.BillRepository;
 import com.example.orders.service.BillService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -19,6 +23,9 @@ import java.util.List;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@Disabled
 @WebMvcTest(BillController.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class BillControllerMockBeanTest {
@@ -31,6 +38,9 @@ public class BillControllerMockBeanTest {
 
     @MockBean
     private BillService billService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void getAllBills_withValidBills_returnOk() throws Exception {
@@ -49,6 +59,142 @@ public class BillControllerMockBeanTest {
                         .get("/api/bills")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$").isNotEmpty());
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(2));
+    }
+
+    @Test
+    void getAllBills_withNone_returnEmptyList() throws Exception {
+
+        List<BillDto> bills = new ArrayList<BillDto>();
+
+        Mockito.when(billService.getAllBills()).thenReturn(bills);
+
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/bills")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isEmpty());
+    }
+
+    @Test
+    void getBillById_whitValidBillDto_returnOk() throws Exception{
+        Long id = 1L;
+        BillDto response = BillDto.builder().id(id).dateBill(new Date()).totalAmount(100.0).build();
+
+        Mockito.when(billService.getBillById(id)).thenReturn(response);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/bills/" + id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(id));
+    }
+
+    @Test
+    void getBillById_whitInvalidBillDto_throwException() throws Exception{
+
+        Long id = 1L;
+
+        Mockito.when(billService.getBillById(id)).thenThrow(new NotFoundException("Not Found Exception", "Bill with id "+id+" does not exists"));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/bills/" + id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void saveBill_withValidBillDto_returnOk() throws Exception{
+
+        Long id = 1L;
+        BillDto response = BillDto.builder().id(id).dateBill(new Date()).totalAmount(100.0).build();
+
+        Mockito.when(billService.saveBill(ArgumentMatchers.any(BillDto.class))).thenReturn(response);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/bills")
+                        .content(objectMapper.writeValueAsString(response))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(id));
+    }
+
+    @Test
+    void saveBill_withInvalidBillDto_returnOk() throws Exception {
+
+        Long id = 1L;
+        BillDto response = BillDto.builder().id(id).dateBill(new Date()).totalAmount(null).build();
+
+        Mockito.when(billService.saveBill(ArgumentMatchers.any(BillDto.class))).thenThrow(new RuntimeException());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/bills")
+                        .content(objectMapper.writeValueAsString(response))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isInternalServerError());
+    }
+
+    @Test
+    void deleteBill_withExistentBillDto_returnAccepted() throws Exception{
+
+        Long id = 1L;
+
+        Mockito.doNothing().when(billService).deleteBill(id);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/bills/{id}",id))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isAccepted());
+
+    }
+
+    @Test
+    void deleteBill_withNotExistentBillDto_throwNotFoundException() throws Exception{
+
+        Long id = 1L;
+
+        Mockito.doThrow(new NotFoundException("Not Found Exception","There is not Bill with id "+id)).when(billService).deleteBill(id);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/bills/{id}",id))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+    }
+
+    @Test
+    void updateBill_withValidBillDto_returnOk() throws Exception{
+
+        Long id = 1L;
+        BillDto response = BillDto.builder().id(id).dateBill(new Date()).totalAmount(200.0).build();
+
+        Mockito.when(billService.updateBill(ArgumentMatchers.any(BillDto.class))).thenReturn(response);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/bills")
+                    .content(objectMapper.writeValueAsString(response))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+    }
+
+    @Test
+    void updateBill_withInvalidBillDto_throwNotFoundException() throws Exception{
+
+        Long id = 1L;
+        BillDto response = BillDto.builder().id(id).dateBill(new Date()).totalAmount(200.0).build();
+
+        Mockito.when(billService.updateBill(ArgumentMatchers.any(BillDto.class))).thenThrow(new NotFoundException("Not Found Exception","There is not Bill with id 1"));
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/bills")
+                    .content(objectMapper.writeValueAsString(response))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
     }
 }
