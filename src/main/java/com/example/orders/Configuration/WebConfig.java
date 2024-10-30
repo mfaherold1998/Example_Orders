6e5a67317a75d7b863f4fb07ec4b481481ac301b
mfaherold1998 <mfaherold1998@gmail.com>
2024-10-30 11:28:36 +0100

    Error with logging form

---
+++ src/main/java/com/example/orders/Configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.orders.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".html"); // O el sufijo que uses (.html, .jsp, etc.)
+        registry.viewResolver(resolver);
+    }
+}
